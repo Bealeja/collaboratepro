6f6885c66fe9e1f0aa85b59b9da2f492dde4b7f3
Bealeja <jack.a.beale@gmail.com>
2023-11-11 20:23:04 +1100

    basic layout

---
+++ src/app/dashboard/clientportal/page.tsx
@@ -0,0 +1,5 @@
+const ClientPortal = () => {
+  return <h1>ClientPortal</h1>;
+};
+
+export default ClientPortal;
---
+++ src/app/dashboard/sidenav/page.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const SideNav = () => {
+  return (
+    <div className="">
+      <button>Toggle Sidebar</button>
+    </div>
+  );
+};
+
+export default SideNav;
